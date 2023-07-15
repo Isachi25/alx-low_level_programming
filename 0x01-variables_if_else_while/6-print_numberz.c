c305a862f9d192c3dc229cd3b2d04cf7010ea05a
Isachi25 <Isachi25>
2023-07-15 14:51:12 -0700

    prints all single digit numbers of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all numberz of base 10
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
