c7948d8534d709a0795afb890a4c6e2ad3cca163
Isachi25 <Isachi25>
2023-07-18 16:55:37 -0700

    prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
