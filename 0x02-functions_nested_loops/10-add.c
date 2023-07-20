18e29d36a415e045068144cee0a3e4689cb27713
Isachi25 <Isachi25>
2023-07-20 03:42:16 -0700

    A function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A function that addstwo integers and returns the result.
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Interger value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * times_table - A function that prints the 9 times table, starting with 0
+ * Return:
+ */
+void times_table(void)
+{
+	 
