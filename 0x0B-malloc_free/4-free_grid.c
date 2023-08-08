dec51b2f904331c6fb2e1fa270b12d9c28dcafe7
Isachi25 <Isachi25>
2023-08-08 16:36:32 -0700

    Frees a 2 dimensional grid previously created

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_grid - 
- *
- *
+ *free_grid - frees 2d array
+ *@grid: 2d grid
+ *@height: height dimension of grid
+ *Description: free memory of grid
+ *Return: nothing
  */
 void free_grid(int **grid, int height)
 {
-	int 
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
