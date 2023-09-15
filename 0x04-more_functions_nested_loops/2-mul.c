9e78603156e2629332696c3654eae68c6d7301ab
Isachi25 <Isachi25>
2023-09-15 05:32:09 -0700

    The product of two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - gives the product of two integers
+ * @a: 1st digit
+ * @b: 2nd digit
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
