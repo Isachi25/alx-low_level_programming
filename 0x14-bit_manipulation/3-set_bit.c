7b205482bde0f2e4e5f661e92d189e418338a1b1
ACHIENG SHARON MASIGA <134079083+Isachi25@users.noreply.github.com>
2023-10-07 01:00:52 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
+ * set_bit - sets a bit at a respective index to 1
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
