778a364dff89a1134d5efab2fc8ba4d3b71d23bd
Isachi25 <sharonachiengmasiga@gmail.com>
2024-05-07 05:45:03 -0700

    0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * linear_search -  searches for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located, or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,13 @@
+### 0x1E. C - Search Algorithms
+
+Objectives;
+* What is a search algorithm
+* What is a linear search
+* What is a binary search
+* What is the best search algorithm to use depending on your needs
+
+
+A search algorithm is a step-by-step procedure used to locate specific data within a collection of data.
+It systematically explores a problem space to find a solution.
+Linear search algorithms check every record for the one associated with a target key in a linear fashion.
+Binary, or half-interval, searches repeatedly target the center of the search structure and divide the search space in half.
